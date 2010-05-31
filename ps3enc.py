6c7706531da740cb01ebc7c2b5334a710c422d23
Alex Bennee <alex@bennee.com>
2010-05-31 18:53:32 +0100

    Do crop detection phase

--- ps3enc.py
+++ ps3enc.py
@@ -15,11 +15,78 @@
 import os
 import sys
 import getopt
+import subprocess
+import re
 
 verbose=0
 x264_encode_opts="-x264encopts subq=6:bframes=3:partitions=p8x8,b8x8,i4x4:weight_b:threads=1:nopsnr:nossim:frameref=3:mixed_refs:level_idc=41:direct_pred=auto:trellis=1"
 
+
+mplayer_bin="/usr/bin/mplayer"
+mencoder_bin="/usr/bin/mencoder"
+
 me=os.path.basename(sys.argv[0])
+passes=3
+
+def guess_best_crop(file):
+    """
+    Calculate the best cropping parameters to use by looking over the whole file
+    """
+    size = os.path.getsize(file)
+    potential_crops = {}
+    for i in range(0, size-size/10, size/10):
+        crop_cmd = mplayer_bin+" -nosound -vo null -sb "+str(i)+" -frames 10 -vf cropdetect "+file
+        if verbose:
+            print "Running: "+crop_cmd
+        try:
+            p = subprocess.Popen(crop_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+            (out, err) = p.communicate()
+            m = re.search("\-vf crop=[-0123456789:]*", out)
+            try:
+                potential_crops[m.group(0)]+=1
+            except KeyError:
+                potential_crops[m.group(0)]=1
+                if verbose:
+                    print "Found:"+m.group(0)
+        except OSError:
+            print "Failed to spawn: "+crop_cmd
+
+    # most common crop?
+    crop_count = 0
+    for crop  in potential_crops:
+        if potential_crops[crop] > crop_count:
+            crop_count = potential_crops[crop]
+            crop_to_use = crop
+
+    if verbose:
+        print "Crop to use is:"+crop_to_use
+
+    return crop_to_use
+
+def do_turbo_pass(file):
+    """
+    Do a fast turbo pass encode of the file
+    """
+    
+
+
+def process_input(file):
+    if verbose:
+        print "Handling: "+a
+
+    crop = guess_best_crop(file)
+
+    if passes>1:
+        do_turbo_pass(file)
+        for i in range(2, passes):
+            do_encoding_pass(file)
+    else:
+        do_encoding_pass(file)
+    
+
+        
+    
+    
 
 
 def usage():
@@ -47,3 +114,6 @@ if __name__ == "__main__":
         if o in ("-v", "--verbose"):
             verbose=1
 
+    for a in args:
+        process_input(os.path.abspath(a))
+        
